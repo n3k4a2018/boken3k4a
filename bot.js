e89ca84316ebd9b3ae5d978f98338eff03e95498
n3k4a2018 <42242199+n3k4a2018@users.noreply.github.com>
2018-09-13 04:08:17 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,73 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '.'
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Nothing`,"https://www.twitch.tv/n3k4a")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+client.on("message", message => {
+
+  function discoRole() {
+    let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    roles.forEach((role) => {
+      let theRole = message.guild.roles.find("name", role);
+      theRole.edit({color: random}).catch(e => {
+        return message.channel.send(":x: **Error:** The role you specified in the `config.json` is either not a role on this server, or his a role higher than the highest role that I have.");
+      });
+    });
+  }
+
+  if(message.content.startsWith(prefix + "startdisco")) {
+    if(allowedUsers.includes(message.author.id)) {
+    setInterval(() => { discoRole(); }, config.ms);
+    message.channel.send("```css\nDiscoing...```");
+    message.channel.send("Please make sure you read the README, you could get IP banned from discord because of ratelimits.");
+  } else {
+    message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
+  }
+} else
+
+if(message.content.startsWith(prefix + "stopdisco")) {
+  if(allowedUsers.includes(message.author.id)) {
+  message.channel.send("I've stopped discoing.");
+  setTimeout(() => { console.log(process.exit(0)); }, 300);
+} else {
+  message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
+  }
+}
+
+});
+const config = require('config.json');
+const prefix = config.prefix;
+
+const allowedUsers = config.allowedUsers;
+const roles = config.roleToDisco;
+
+client.on("ready", () => {
+    client.user.setPresence({ game: { name: `Disco Roles! Created by i am @💔🎧⦕n⦖⦕3⦖⦕k⦖⦕4⦖🎧💔#2050 ` }, type: 0 });
+    console.log("Disco role bot online! Created by n3k4a.");
+});
+client.login(process.env.BOT_TOKEN);
