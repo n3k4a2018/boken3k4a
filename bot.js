5ede77082367ca7114f3baec62fd3e828045ff14
n3k4a2018 <42242199+n3k4a2018@users.noreply.github.com>
2018-09-13 04:38:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,3 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '.'
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"https://www.twitch.tv/n3k4a")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
-
 client.on("message", message => {
 
   function discoRole() {
@@ -70,4 +40,3 @@ client.on("ready", () => {
     client.user.setPresence({ game: { name: `Disco Roles! Created by i am @💔🎧⦕n⦖⦕3⦖⦕k⦖⦕4⦖🎧💔#2050 ` }, type: 0 });
     console.log("Disco role bot online! Created by n3k4a.");
 });
-client.login(process.env.BOT_TOKEN);
